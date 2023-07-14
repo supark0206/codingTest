0845b14043f46665791e12cd15aeb3b2d8018fb0
supark0206 <supark0206@gmail.com>
2023-07-14 15:42:45 +0900

    DFS BFS 활용 진행중

---
+++ src/DFS_BFS_08/미로찾기_BFS/Main.java
@@ -0,0 +1,54 @@
+package DFS_BFS_08.미로찾기_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int[] dx={-1,0,1,0};
+    static int[] dy={0,-1,0,1};
+    static int[][] board, dis;
+
+    public void BFS(int x, int y){
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x,y));
+        board[x][y] = 1;
+        while (!queue.isEmpty()) {
+            Point tmp = queue.poll();
+            for(int i=0; i<4; i++){
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+
+                if(nx>=1 && nx<7 && ny>=1 && ny<=7 && board[nx][ny]==0){
+                    board[nx][ny]=1;
+                    queue.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+        board = new int[8][8];
+        for(int i=1; i<=7; i++){
+            for(int j=1; j<=7; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+        board[1][1]=1;
+        main.BFS(1,1);
+        if(dis[7][7]==0) System.out.println(-1);
+        else System.out.println(dis[7][7]);
+
+    }
+}
+class Point{
+    public int x,y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
